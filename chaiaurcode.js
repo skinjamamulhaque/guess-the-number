f25989f08f8479373bbd044f2853c7e097d853d7
Sk Injamamul Haque <injamamulhaque76@gmail.com>
2024-03-19 11:01:09 +0530

    bugs and error fixed

--- chaiaurcode.js
+++ chaiaurcode.js
@@ -13,13 +13,17 @@ let numGuses = 1;
 let playGame = true;
 
 
+
+const userGuessEvent = function(e){
   if(playGame){
-    SubmitBtn.addEventListener('click', function (e) {
-      e.preventDefault();
-      let userGuess = parseInt(userInput.value);
-      validateGuess(userGuess);
-    });
+    e.preventDefault();
+    let userGuess = parseInt(userInput.value);
+    validateGuess(userGuess);
   }
+}
+
+SubmitBtn.addEventListener('click', userGuessEvent)
+ 
 
 
 
@@ -27,6 +31,7 @@ let playGame = true;
 function validateGuess(userGuess) {
   if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
     alert('Please enter a valid number between 1 to 100');
+    // displayMsg(`Please enter a valid number between 1 to 100`)
   } else {
     prevGuess.push(userGuess);
     if(numGuses > 10 ){
@@ -40,8 +45,12 @@ function validateGuess(userGuess) {
 
 function checkGuess(userGuess){
   if(userGuess === randomNumber){
-    alert('you won the game')
+    displayMsg(`You won the Game. Random number was ${randomNumber}`)
   }
+}
+
+function displayMsg(msg){
+  suggestionGuess.innerHTML = `<h3>${msg}</h3>`
 }
 
 function displayGuess(guess) {
@@ -54,30 +63,34 @@ function displayGuess(guess) {
 function endGame(){
   userInput.value = "";
   userInput.setAttribute ('disabled', '')
+  SubmitBtn.setAttribute ('disabled', '')
   playGame = false;
   btnAction()
 }
 
 function btnAction(){
   SubmitBtn.value = 'Game Over'
+  SubmitBtn.removeEventListener('click', userGuessEvent)
   setTimeout(function(){
     SubmitBtn.value = 'Start New Game'
-    gameStart()
+    SubmitBtn.removeAttribute('disabled', '')
+    SubmitBtn.addEventListener('click', gameStart)
   },1000)
 }
 
-function gameStart(){
-  // SubmitBtn.removeAttribute ('disabled', '')
-  if(SubmitBtn.value === 'Start New Game' ){
-    SubmitBtn.addEventListener('click', function(e){
-      SubmitBtn.value = "submit Guess"
-      userInput.removeAttribute ('disabled', '')
-      randomNumber = parseInt(Math.random() * 100 + 1);
-    prevGuess = [];
-    numGuses = 1;
-    guessSlots.innerHTML = '';
-    remainingGuesses.innerHTML = `${11 - numGuses} `;
-      playGame = true;
-    })
-  }  
+
+
+function gameStart(e){
+  e.preventDefault()
+  SubmitBtn.value = "submit Guess"
+  userInput.removeAttribute ('disabled', '')
+  randomNumber = parseInt(Math.random() * 100 + 1);
+  // console.log(randomNumber)
+  prevGuess = [];
+  numGuses = 1;
+  guessSlots.innerHTML = '';
+  remainingGuesses.innerHTML = `${11 - numGuses} `;
+  playGame = true;
+  SubmitBtn.removeEventListener('click', gameStart)
+  SubmitBtn.addEventListener('click', userGuessEvent)
 }
